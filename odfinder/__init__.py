109058cf1c79c18a903fb011b2cb7c9f31e36ebe
jact <jachavar@gmail.com>
2020-04-21 19:22:51 +0200

    Python3 code evolution

--- odfinder/__init__.py
+++ odfinder/__init__.py
@@ -17,13 +17,11 @@
 
 import locale
 import gettext
-import __builtin__
-__builtin__._ = gettext.gettext
 
 LOCALE_PATH = '/usr/share/locale'
 DOMAIN = 'odfinder'
 
-gettext.install(DOMAIN, LOCALE_PATH, unicode=1)
+gettext.install(DOMAIN, LOCALE_PATH)
 
 gettext.bindtextdomain(DOMAIN, LOCALE_PATH)
 if hasattr(gettext, 'bind_textdomain_codeset'):
@@ -34,13 +32,3 @@ locale.bindtextdomain(DOMAIN, LOCALE_PATH)
 if hasattr(locale, 'bind_textdomain_codeset'):
     locale.bind_textdomain_codeset(DOMAIN, 'UTF-8')
 locale.textdomain(DOMAIN)
-
-# http://www.ianbicking.org/illusive-setdefaultencoding.html
-# begin unicode hack
-import sys
-
-if sys.getdefaultencoding() != 'utf-8':
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
-    # now default enconding is 'utf-8' ;)
-# end unicode hack
